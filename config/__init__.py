e2c2a894759db19df0d458e386269a7b6883d102
trevorbchen <trevorbchen@gmail.com>
2025-09-30 08:27:20 -0700

    update

--- config/__init__.py
+++ config/__init__.py
@@ -6,7 +6,7 @@ This package contains configuration settings and environment management.
 Components:
 - settings: Application settings loaded from environment
 """
-
+    
 from config.settings import settings
 
 __all__ = ['settings']
